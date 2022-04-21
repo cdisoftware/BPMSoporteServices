342735d9de8850a1a3a6172a76f59d205fc355bb
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2022-04-21 15:02:58 -0500

    Ajustes

--- src/main/java/com/cdi/com/BPMSoporte/Controller/Controller.java
+++ src/main/java/com/cdi/com/BPMSoporte/Controller/Controller.java
@@ -1,7 +1,43 @@
 
 package com.cdi.com.BPMSoporte.Controller;
 
+import com.cdi.com.BPMSoporte.Entity.BpmNet_ConsultarsoporteEntity;
+import com.cdi.com.BPMSoporte.Service.BpmNet_ConsultarsoporteService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/", produces = "application/json", method = {RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
 
 public class Controller {
+    @Autowired
+    BpmNet_ConsultarsoporteService serviceBpmNet_ConsultarsoporteService;
     
+    @GetMapping("/consultaSoporteBpm/{TICKET}/{NOMBRE}/{TRAMITESOPORTE}/{FUNCIONARIOREPORTA}/{RAZONSOCIAL}/{CDESTADO}/{EVENTO}/{CDMODULO}/{TIPOSOLICITUD}/{PRIORIDAD}/{CDIMPUTACION}/{FECHAREGISTROINI}/{FECHAREGISTROFIN}/{FECHAATENCIONINI}/{FECHAATENCIONFIN}/{FUNCIONARIOSOLUCIO}")
+    public List<BpmNet_ConsultarsoporteEntity> ConsultaBackup(
+            @PathVariable String TICKET,
+            @PathVariable String NOMBRE,
+            @PathVariable String TRAMITESOPORTE,
+            @PathVariable String FUNCIONARIOREPORTA,
+            @PathVariable String RAZONSOCIAL,
+            @PathVariable String CDESTADO,
+            @PathVariable String EVENTO,
+            @PathVariable String CDMODULO,
+            @PathVariable String TIPOSOLICITUD,
+            @PathVariable String PRIORIDAD,
+            @PathVariable String CDIMPUTACION,
+            @PathVariable String FECHAREGISTROINI,
+            @PathVariable String FECHAREGISTROFIN,
+            @PathVariable String FECHAATENCIONINI,
+            @PathVariable String FECHAATENCIONFIN,
+            @PathVariable String FUNCIONARIOSOLUCIO) {
+        return serviceBpmNet_ConsultarsoporteService.ConsultaSoporte(TICKET, NOMBRE, TRAMITESOPORTE, FUNCIONARIOREPORTA,RAZONSOCIAL, CDESTADO, EVENTO, CDMODULO, TIPOSOLICITUD, PRIORIDAD, CDIMPUTACION, FECHAREGISTROINI, FECHAREGISTROFIN, FECHAATENCIONINI, FECHAATENCIONFIN, FUNCIONARIOSOLUCIO);
+    }
 }
